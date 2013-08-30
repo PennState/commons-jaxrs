35f09703edd4ff5cd84e583260801cd1ad0cad0a
Shawn Eion Smith <ses44@psu.edu>
2013-08-30 16:36:10 -0400

    Made Pair serializable

--- src/main/java/edu/psu/util/collection/Pair.java
+++ src/main/java/edu/psu/util/collection/Pair.java
@@ -1,7 +1,11 @@
 package edu.psu.util.collection;
 
-public class Pair<E, F>
+import java.io.Serializable;
+
+public class Pair<E, F> implements Serializable
 {
+  private static final long serialVersionUID = 1247816543522165967L;
+  
   public Pair(E e, F f)
   {
     first = e;
