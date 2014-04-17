6827a3c7a87f0c45f5123876655f9966c8687196
ses44 <ses44@psu.edu>
2014-04-17 10:41:20 -0400

    Adding JAXB annotations to the Pair Object

--- src/main/java/edu/psu/util/collection/Pair.java
+++ src/main/java/edu/psu/util/collection/Pair.java
@@ -2,6 +2,13 @@ package edu.psu.util.collection;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="pair")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Pair<E, F> implements Serializable
 {
   private static final long serialVersionUID = 1247816543522165967L;
@@ -12,6 +19,9 @@ public class Pair<E, F> implements Serializable
     second = f;
   }
 
+  @XmlElement(name="lhs")
   public E first;
+  
+  @XmlElement(name="rhs")
   public F second;
 }
