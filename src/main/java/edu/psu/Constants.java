15797e3782500324a88bb9f68732d6092e2fd343
Michael Zalewski <mez5001@psu.edu>
2014-05-12 13:02:37 -0400

    Added initial Constants values

--- src/main/java/edu/psu/Constants.java
+++ src/main/java/edu/psu/Constants.java
@@ -1,6 +1,20 @@
 package edu.psu;
 
-public class Constants
+/**
+ * Class to hold Common Constants
+ * 
+ * @author mez5001
+ *
+ */
+public final class Constants
 {
 
+  public static final String USER_ID = "Userid";
+  
+  //Not Found Strings
+  public static final String NOT_FOUND = " was not found";
+  
+  //Colon Strings
+  public static final String COLON = ":";
+  public static final String COLON_WS = " : ";
 }
