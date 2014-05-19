a5575cc4a77cda96e62dcd34d08425634cfa672f
Steve Moyer <smoyer@psu.edu>
2014-05-19 12:18:32 -0400

    Added a RESTful response message that allows a value to be passed along with a set of external links.

---
+++ src/main/java/edu/psu/rest/HateoasMessage.java
@@ -0,0 +1,69 @@
+package edu.psu.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response.Status;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+@XmlRootElement(name = "hateoas-message")
+public class HateoasMessage<T> {
+  
+  @XmlElement(name="status")
+  @XmlJavaTypeAdapter(XmlStatusAdapter.class)
+  private Status status_;
+  
+  @XmlElement(name = "value")
+  private T value_;
+  
+  @XmlElementWrapper(name = "reference-list")
+  private List<AtomLink> references_;
+  
+  public HateoasMessage() {
+    // Required no-argument constructor
+  }
+  
+  public HateoasMessage(Status status) {
+    status_ = status;
+  }
+  
+  public HateoasMessage(Status status, T value) {
+    status_ = status;
+    value_ = value;
+  }
+
+  public Status getStatus() {
+    return status_;
+  }
+
+  public void setStatus(Status status) {
+    status_ = status;
+  }
+
+  public T getValue() {
+    return value_;
+  }
+
+  public void setValue(T value) {
+    value_ = value;
+  }
+
+  public List<AtomLink> getReferences() {
+    return references_;
+  }
+
+  public void setReferences(List<AtomLink> references) {
+    references_ = references;
+  }
+  
+  public void addReference(AtomLink reference) {
+    if(references_ == null) {
+      references_ = new ArrayList<AtomLink>();
+    }
+    references_.add(reference);
+  }
+
+}
