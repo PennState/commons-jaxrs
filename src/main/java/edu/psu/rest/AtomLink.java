de9bc9691a6c1bd826f06b9f1f881de5bb7d8901
Steve Moyer <smoyer@psu.edu>
2014-05-19 11:38:00 -0400

    Added a class to represent an Atom link so that HATEOAS compliant responses can use a standardized and consistent format for reference/external links.

---
+++ src/main/java/edu/psu/rest/AtomLink.java
@@ -0,0 +1,75 @@
+package edu.psu.rest;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Implements the Atom Link Model for HATEOAS resource references.
+ * 
+ * http://www.xml.com/pub/a/2004/06/16/dive.html
+ * http://timelessrepo.com/haters-gonna-hateoas#hateoas
+ * 
+ * @author Steve Moyer <smoyer@psu.edu>
+ */
+@XmlRootElement(name = "link")
+@XmlAccessorType(XmlAccessType.NONE)
+public class AtomLink {
+  
+  @XmlElement(name = "href")
+  private String hyperlink_;
+  
+  @XmlElement(name = "rel")
+  private String relation_;
+  
+  @XmlElement(name = "title", nillable = true)
+  private String title_;
+  
+  @XmlElement(name = "type")
+  private String type_;
+  
+  public AtomLink(String relation, String type, String hyperlink) {
+    relation_ = relation;
+    type_ = type;
+    hyperlink_ = hyperlink;
+  }
+  
+  public AtomLink(String relation, String type, String hyperlink, String title) {
+    this(relation, type, hyperlink);
+    title_ = title;
+  }
+  
+  public String getHyperlink() {
+    return hyperlink_;
+  }
+
+  public void setHyperlink(String hyperlink) {
+    hyperlink_ = hyperlink;
+  }
+
+  public String getRelation() {
+    return relation_;
+  }
+
+  public void setRelation(String relation) {
+    relation_ = relation;
+  }
+
+  public String getTitle() {
+    return title_;
+  }
+
+  public void setTitle(String title) {
+    title_ = title;
+  }
+
+  public String getType() {
+    return type_;
+  }
+
+  public void setType(String type) {
+    type_ = type;
+  }
+  
+}
