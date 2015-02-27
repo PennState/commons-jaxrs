6ad12c8f8df10ff39caffa2b36ab01606e747a10
ses44 <ses44@psu.edu>
2015-02-27 15:17:05 -0500

    Adding a validator for ISO Countries

---
+++ src/main/java/edu/psu/injection/validator/IsoCountry.java
@@ -0,0 +1,20 @@
+package edu.psu.injection.validator;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import javax.validation.Constraint;
+
+@Constraint(validatedBy = IsoCountryValidator.class)
+@Target( { METHOD, FIELD, PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface  IsoCountry 
+{
+  String message() default "Country does not exist in the ISO standard";
+	 
+  Class<?>[] groups() default {};
+}
