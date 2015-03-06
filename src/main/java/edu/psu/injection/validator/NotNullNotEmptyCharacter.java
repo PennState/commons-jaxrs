13525a9b071f1a038cf2914cb68acd1e1f422226
ses44 <ses44@psu.edu>
2015-03-06 14:53:59 -0500

    Adding Payload values to the validators

--- src/main/java/edu/psu/injection/validator/NotNullNotEmptyCharacter.java
+++ src/main/java/edu/psu/injection/validator/NotNullNotEmptyCharacter.java
@@ -3,11 +3,13 @@ package edu.psu.injection.validator;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Constraint(validatedBy = NotNullNotEmptyCharacterValidator.class)
 @Target( { METHOD, FIELD, PARAMETER })
@@ -17,4 +19,6 @@ public @interface  NotNullNotEmptyCharacter
   String message() default "Character field cannot be null or empty";
 	 
   Class<?>[] groups() default {};
+  
+  Class<? extends Payload>[] payload() default {};
 }
