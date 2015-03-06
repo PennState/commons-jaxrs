4267fa8060c6683c35e65786b4192ff1210d0f99
ses44 <ses44@psu.edu>
2015-03-06 08:27:43 -0500

    Added the NotNullNotEmpty validator

---
+++ src/main/java/edu/psu/injection/validator/NotNullNotEmpty.java
@@ -0,0 +1,20 @@
+package edu.psu.injection.validator;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import javax.validation.Constraint;
+
+@Constraint(validatedBy = NotNullNotEmptyValidator.class)
+@Target( { METHOD, FIELD, PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface  NotNullNotEmpty 
+{
+  String message() default "String cannot be null or empty";
+	 
+  Class<?>[] groups() default {};
+}
