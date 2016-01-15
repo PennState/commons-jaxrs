64210b878ce7c78b920a36f9676ed7f734cb698b
Christopher Harm <crh5255@psu.edu>
2016-01-15 15:08:33 -0500

    Adding rest client util

---
+++ src/main/java/edu/psu/rest/RestClientUtil.java
@@ -0,0 +1,21 @@
+package edu.psu.rest;
+
+import javax.ws.rs.core.Response;
+
+public class RestClientUtil {
+
+  private RestClientUtil() {
+    
+  }
+  
+  public static void checkForSuccess(Response response) throws RestClientException {
+    Response.Status.Family responseFamily = response.getStatusInfo().getFamily();
+    
+    if (responseFamily.equals(Response.Status.Family.CLIENT_ERROR)||
+        responseFamily.equals(Response.Status.Family.SERVER_ERROR))
+    {
+      throw new RestClientException(response);
+    }
+  }
+  
+}
