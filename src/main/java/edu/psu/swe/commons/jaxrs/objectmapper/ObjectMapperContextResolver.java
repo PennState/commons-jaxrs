5580d09b62f86103bf15a873d08e83717888944f
Christopher Harm <crh5255@psu.edu>
2016-10-07 11:13:12 -0400

    Updating ObjectMapperContextResolver for the client

--- src/main/java/edu/psu/swe/commons/jaxrs/objectmapper/ObjectMapperContextResolver.java
+++ src/main/java/edu/psu/swe/commons/jaxrs/objectmapper/ObjectMapperContextResolver.java
@@ -1,17 +1,30 @@
 package edu.psu.swe.commons.jaxrs.objectmapper;
 
-import javax.inject.Inject;
 import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Provider;
 
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 
-@Provider
 public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
 
-  @Inject
-  @JaxrsObjectMapper
   private ObjectMapper objectMapper;
+  
+  public ObjectMapperContextResolver() {
+    objectMapper = new ObjectMapper();
+
+    JaxbAnnotationModule jaxbAnnotationModule = new JaxbAnnotationModule();
+    objectMapper.registerModule(jaxbAnnotationModule);
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    AnnotationIntrospector jaxbIntrospector = new JaxbAnnotationIntrospector(objectMapper.getTypeFactory());
+    AnnotationIntrospector jacksonIntrospector = new JacksonAnnotationIntrospector();
+    AnnotationIntrospector pair = new AnnotationIntrospectorPair(jacksonIntrospector, jaxbIntrospector);
+    objectMapper.setAnnotationIntrospector(pair);  }
 
   @Override
   public ObjectMapper getContext(Class<?> type) {
