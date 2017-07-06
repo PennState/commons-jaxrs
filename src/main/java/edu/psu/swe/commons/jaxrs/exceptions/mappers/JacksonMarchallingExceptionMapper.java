e3ca483a32f4a795574e1db9ddae909afa0aad14
Christopher Harm <crh5255@psu.edu>
2017-07-06 16:34:15 -0400

    Adding ExceptionMappers for Bean Validation Exceptions

---
+++ src/main/java/edu/psu/swe/commons/jaxrs/exceptions/mappers/JacksonMarchallingExceptionMapper.java
@@ -0,0 +1,20 @@
+package edu.psu.swe.commons.jaxrs.exceptions.mappers;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import edu.psu.swe.commons.jaxrs.ErrorMessage;
+
+public class JacksonMarchallingExceptionMapper implements ExceptionMapper<InvalidFormatException> {
+
+  @Override
+  public Response toResponse(InvalidFormatException exception) {
+    ErrorMessage em = new ErrorMessage(Status.BAD_REQUEST);
+    em.addErrorMessage(exception.getMessage());
+    return em.toResponse();
+  }
+
+}
