76f5e7b23a4570c8ea9be0e7047906de0418fc62
Alex Haskell <aph12@psu.edu>
2017-08-02 09:11:22 -0400

    Add convenience methods for PatchOperation

--- src/main/java/edu/psu/swe/commons/jaxrs/patch/PatchOperation.java
+++ src/main/java/edu/psu/swe/commons/jaxrs/patch/PatchOperation.java
@@ -9,6 +9,7 @@ import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,20 +20,193 @@ import lombok.EqualsAndHashCode;
 @XmlAccessorType(XmlAccessType.NONE)
 public class PatchOperation {
 
+  /**
+   * Create a generic `add` PatchOperation.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
   public static PatchOperation add(JsonPointer jsonPointer, JsonNode value) {
     return new PatchOperation(Type.ADD, jsonPointer, value);
   }
 
+  /**
+   * Create an `add` PatchOperation for booleans.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation add(JsonPointer jsonPointer, boolean value) {
+    return add(jsonPointer, JsonNodeFactory.instance.booleanNode(value));
+  }
+
+  /**
+   * Create an `add` PatchOperation for longs/ints.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation add(JsonPointer jsonPointer, long value) {
+    return add(jsonPointer, JsonNodeFactory.instance.numberNode(value));
+  }
+
+  /**
+   * Create an `add` PatchOperation for doubles/floats.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation add(JsonPointer jsonPointer, double value) {
+    return add(jsonPointer, JsonNodeFactory.instance.numberNode(value));
+  }
+
+  /**
+   * Create an `add` PatchOperation for Strings.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation add(JsonPointer jsonPointer, String value) {
+    return add(jsonPointer, JsonNodeFactory.instance.textNode(value));
+  }
+
+  /**
+   * Create an `add` PatchOperation for Objects.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation add(JsonPointer jsonPointer, Object value) {
+    return add(jsonPointer, JsonNodeFactory.instance.pojoNode(value));
+  }
+
+  /**
+   * Create a generic `remove` PatchOperation.
+   * @param jsonPointer
+   * @return
+   */
   public static PatchOperation remove(JsonPointer jsonPointer) {
     return new PatchOperation(Type.REMOVE, jsonPointer);
   }
 
+  /**
+   * Create a generic `replace` PatchOperation.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
   public static PatchOperation replace(JsonPointer jsonPointer, JsonNode value) {
     return new PatchOperation(Type.REPLACE, jsonPointer, value);
   }
 
+  /**
+   * Create a `replace` PatchOperation for booleans.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation replace(JsonPointer jsonPointer, boolean value) {
+    return replace(jsonPointer, JsonNodeFactory.instance.booleanNode(value));
+  }
+
+  /**
+   * Create a `replace` PatchOperation for longs/ints.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation replace(JsonPointer jsonPointer, long value) {
+    return replace(jsonPointer, JsonNodeFactory.instance.numberNode(value));
+  }
+
+  /**
+   * Create a `replace` PatchOperation for doubles/floats.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation replace(JsonPointer jsonPointer, double value) {
+    return replace(jsonPointer, JsonNodeFactory.instance.numberNode(value));
+  }
+
+  /**
+   * Create a `replace` PatchOperation for Strings.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation replace(JsonPointer jsonPointer, String value) {
+    return replace(jsonPointer, JsonNodeFactory.instance.textNode(value));
+  }
+
+  /**
+   * Create a `replace` PatchOperation for Objects.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation replace(JsonPointer jsonPointer, Object value) {
+    return replace(jsonPointer, JsonNodeFactory.instance.pojoNode(value));
+  }
+
+  /**
+   * Create a generic `test` PatchOperation.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
   public static PatchOperation test(JsonPointer jsonPointer, JsonNode value) {
     return new PatchOperation(Type.TEST, jsonPointer, value);
+  }
+
+  /**
+   * Create a `test` PatchOperation for booleans.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation test(JsonPointer jsonPointer, boolean value) {
+    return test(jsonPointer, JsonNodeFactory.instance.booleanNode(value));
+  }
+
+  /**
+   * Create a `test` PatchOperation for ints/longs.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation test(JsonPointer jsonPointer, long value) {
+    return test(jsonPointer, JsonNodeFactory.instance.numberNode(value));
+  }
+
+  /**
+   * Create a `test` PatchOperation for doubles/floats.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation test(JsonPointer jsonPointer, double value) {
+    return test(jsonPointer, JsonNodeFactory.instance.numberNode(value));
+  }
+
+  /**
+   * Create a `test` PatchOperation for Strings.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation test(JsonPointer jsonPointer, String value) {
+    return test(jsonPointer, JsonNodeFactory.instance.textNode(value));
+  }
+
+  /**
+   * Create a `test` PatchOperation for Objects.
+   * @param jsonPointer
+   * @param value
+   * @return
+   */
+  public static PatchOperation test(JsonPointer jsonPointer, Object value) {
+    return test(jsonPointer, JsonNodeFactory.instance.pojoNode(value));
   }
 
   @XmlEnum(String.class)
@@ -58,6 +232,9 @@ public class PatchOperation {
   @XmlElement
   @XmlJavaTypeAdapter(JsonPointerAdapter.class)
   private JsonPointer from;
+
+  protected PatchOperation() {
+  }
 
   protected PatchOperation(Type type, JsonPointer pointer, JsonNode value) {
     this.operation = type;
