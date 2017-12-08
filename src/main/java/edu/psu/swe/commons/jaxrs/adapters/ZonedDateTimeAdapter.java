5444cf2ba6ab66617526d15295bd5e0ee263db7b
clp207 <clp207@psu.edu>
2017-12-08 16:24:16 -0500

    added a zoned date time adapter

---
+++ src/main/java/edu/psu/swe/commons/jaxrs/adapters/ZonedDateTimeAdapter.java
@@ -0,0 +1,43 @@
+package edu.psu.swe.commons.jaxrs.adapters;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class ZonedDateTimeAdapter extends XmlAdapter<String, ZonedDateTime> {
+  
+  private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_DATE_TIME;
+
+  @Override
+  public ZonedDateTime unmarshal(String v) throws Exception {
+    if (v == null) {
+      return null;
+    }
+
+    ZonedDateTime zonedDateTime = null;
+    try {
+      zonedDateTime = ZonedDateTime.parse(v, FORMATTER);
+    } catch (DateTimeException e) {
+      // couldn't parse to a ZoneDateTime, try LocalDateTime
+      LocalDateTime dt = LocalDateTime.parse(v, FORMATTER);
+
+      // default to EST
+      zonedDateTime = dt.atZone(ZoneId.systemDefault());
+    }
+
+    return zonedDateTime;
+  }
+
+  @Override
+  public String marshal(ZonedDateTime v) throws Exception {
+    if (v == null) {
+      return null;
+    }
+    return FORMATTER.format(v.withZoneSameInstant(ZoneId.of("UTC")));
+  }
+}
