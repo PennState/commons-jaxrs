08d29d7c6a70be1752af87bc582ce2cc796f4bf7
Michael Zalewski <mez5001@psu.edu>
2018-06-22 10:47:13 -0400

    Renaming package of unit test

--- src/test/java/edu/psu/swe/commons/jaxrs/converters/param/LocalDateConverterProviderTest.java
+++ src/test/java/edu/psu/swe/commons/jaxrs/converters/param/LocalDateConverterProviderTest.java
@@ -1,4 +1,4 @@
-package edu.psu.swe.common.jaxrs.converters.param;
+package edu.psu.swe.commons.jaxrs.converters.param;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
